fbc06d59c7e0e58de5c26667250bf1f6e0dfc797
Asif Ahmed Khan <asifak001@gmail.com>
2026-01-01 15:55:30 -0500

    installed requirements

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # US-Visa-Acceptance-MLOps
 
 Kaggle dataset link: https://www.kaggle.com/datasets/moro23/easyvisa-dataset 
+
+
+
+Anaconda environment setup:
+conda create -n Visa python=3.8 -y
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa",
+    version="0.0.0",
+    author="Asif",
+    author_email="asifak001@gmail.com",
+    packages=find_packages()
+)
