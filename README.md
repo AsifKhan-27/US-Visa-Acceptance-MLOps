f0c3a1712554dc1e66762bd779590d40ebcea775
Asif Ahmed Khan <asifak001@gmail.com>
2026-01-03 21:34:28 -0500

    completed data ingestion from csv

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ conda create -n Visa python=3.8 -y
 
 The dataset will be stored in the database.
 Data ingestion will get data from the database.
+
+
+## Workflow:
+
+1. constants
+2. entity
+3. components
+4. Pipeline
--- demo.py
+++ demo.py
@@ -1,11 +1,18 @@
 from US_Visa.logger import logging
 from US_Visa.exception import USVisaException
 import sys
+from US_Visa.pipeline.training_pipeline import TrainPipeline
 
 logging.info("Welcome to the custom log")
 
 
-try:
-    a = 2/0
-except Exception as e:
-    raise USVisaException(e, sys)    
+# try:
+#     a = 2/0
+# except Exception as e:
+#     raise USVisaException(e, sys)
+
+
+obj = TrainPipeline()
+obj.run_pipeline()
+
+logging.info('Finished running demo.py')
